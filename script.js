93fe125b2740c23b8729e8b0c25229d7d917c7f3
drenrr <drenkpurdha@gmail.com>
2025-02-13 19:11:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("passwordForm").addEventListener("submit", function (event) {
         event.preventDefault(); // Prevent form from actually submitting
         var passwordInput = document.getElementById("passwordInput").value; // Corrected ID
-        var correctPassword = "yourpassword"; // Change this to your actual password
+        var correctPassword = "06062024"; // Change this to your actual password
         if (passwordInput === correctPassword) {
             window.location.href = "main.html"; // Redirect to main page
         } else {
