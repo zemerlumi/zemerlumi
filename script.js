b3ff9162225d6e8cce691a80b255e18e2f069ff7
drenrr <drenkpurdha@gmail.com>
2025-02-14 00:04:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("passwordForm").addEventListener("submit", function (e) {
-        e.preventDefault();
-        const password = document.getElementById("passwordInput").value;
-        // CORRECT PASSWORD SET TO 08062024
-        if (password === "08062024") {
-            window.location.href = "main.html";
-        } else {
-            alert("Gabim! Provoni përsëri ❤️");
-        }
-    });
+// Password Validation
+document.getElementById("passwordForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const password = document.getElementById("passwordInput").value;
+    
+    if(password === "08062024") {
+        window.location.href = "main.html";
+    } else {
+        alert("Gabim! Provoni përsëri ❤️");
+        document.getElementById("passwordInput").value = "";
+    }
 });
